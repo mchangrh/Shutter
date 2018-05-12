22cb001adcc1e8f778a4e64856c3ed6904021bb3
Michael M. Chang <mchangrh@gmail.com>
2018-05-11 19:55:43 -0500

    Fixed patch script

--- Patch/automated.sh
+++ Patch/automated.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Links
-DNSMASQ = "https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/05-restrict.conf"
-GRAVITY = "https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/gravity.diff"
-HOSTS = "https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/hosts"
+DNSMASQ="https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/05-restrict.conf"
+GRAVITY="https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/gravity.diff"
+HOSTS="https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/hosts"
 
 
 # dnsmasq file for safesearch
