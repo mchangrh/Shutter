83e184cb26fa5797db7d88bffffaaf4c563fbdab
Michael M. Chang <mchangrh@gmail.com>
2018-05-11 20:06:46 -0500

    Fixed links that were breaking curl and wget

--- Patch/automated.sh
+++ Patch/automated.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-# Links
-DNSMASQ="https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/05-restrict.conf"
-GRAVITY="https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/gravity.diff"
-HOSTS="https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/hosts"
-
-
 # dnsmasq file for safesearch
-wget -O /etc/dnsmasq.d/05-restrict.conf $DNSMASQ
-
+wget -O /etc/dnsmasq.d/05-restrict.conf https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/05-restrict.conf
 # patch gravity.sh
-curl $GRAVITY | patch /opt/pihole/gravity.sh
-
+curl https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/gravity.diff | patch /opt/pihole/gravity.sh
 # Add to hosts file
-curl $HOSTS >> /etc/hosts
+curl https://raw.githubusercontent.com/mchangrh/Shutter/master/Patch/hosts >> /etc/hosts
